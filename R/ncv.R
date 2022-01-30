84f10f502be4e23dc58f9773e10e55552fcdcd9a
Trevor Glynn <tfglynn@protonmail.com>
2022-01-29 21:15:43 -0600

    Add NCV and new plots

--- R/ncv.R
+++ R/ncv.R
@@ -1,42 +1,89 @@
-#d <- cancer_dataset
-#
-#X <- select(d, -class)
-#Y <- d$class
-#
-#misclass_loss <- function(y1, y2, funcs_params = NA) y1 != y2
-#
-#fitter_rand_forest <-
-#  function(X, Y, idx = NA, funcs_params) {
-#    if (any(is.na(idx))) idx <- 1:nrow(X)
-#    classes <- funcs_params$classes
-#    rf_spec <- rand_forest(mode = "classification")
-#    XY <- cbind(X[idx, ], class = Y[idx])
-#    fit(rf_spec, class ~ ., XY)
-#  }
-#
-#predictor_rand_forest <-
-#  function(model, X_new, funcs_params = NA) {
-#    predict(model, X_new)$.pred_class
-#  }
-#
-#rand_forest_fns <-
-#  list(fitter = fitter_rand_forest,
-#       predictor = predictor_rand_forest,
-#       loss = misclass_loss,
-#       name = "rand_forest")
-#
-#nested_cv_results <-
-#  nested_cv(X, Y, rand_forest_fns, n_folds = 5, reps = 10,
-#            funcs_params = list(classes = levels(d$class)),
-#            verbose = TRUE)
-#
-#naive_cv_results <-
-#  naive_cv(X, Y, rand_forest_fns, n_folds = 5,
-#            funcs_params = list(classes = levels(d$class)))
-#
-#print(paste0("Nested CV interval: (", 
-#             1-nested_cv_results$ci_lo, ", ",
-#             1-nested_cv_results$ci_hi, ")"))
-#print(paste0("Standard CV interval: (", 
-#             1-naive_cv_results$ci_lo, ", ",
-#             1-naive_cv_results$ci_hi, ")"))
+d <- chess_dataset
+misclass_loss <- function(y1, y2, funcs_params = NA) y1 != y2
+
+fitter_naive_bayes <-
+  function(X, Y, idx = NA, funcs_params) {
+    if (any(is.na(idx))) idx <- 1:nrow(X)
+    classes <- funcs_params$classes
+    rf_spec <- rand_forest(mode = "classification")
+    XY <- cbind(X[idx, ], class = Y[idx])
+    naive_bayes(class ~ ., XY, laplace = 1)
+  }
+
+fitter_rand_forest <-
+  function(X, Y, idx = NA, funcs_params) {
+    if (any(is.na(idx))) idx <- 1:nrow(X)
+    classes <- funcs_params$classes
+    rf_spec <- rand_forest(mode = "classification")
+    XY <- cbind(X[idx, ], class = Y[idx])
+    fit(rf_spec, class ~ ., XY)
+  }
+
+predictor_naive_bayes <-
+  function(model, X_new, funcs_params = NA) {
+    predict(model, X_new)
+  }
+
+predictor_rand_forest <-
+  function(model, X_new, funcs_params = NA) {
+    predict(model, X_new)$.pred_class
+  }
+
+naive_bayes_fns <-
+  list(fitter = fitter_rand_forest,
+       predictor = predictor_rand_forest,
+       loss = misclass_loss,
+       name = "naive_bayes")
+
+rand_forest_fns <-
+  list(fitter = fitter_rand_forest,
+       predictor = predictor_rand_forest,
+       loss = misclass_loss,
+       name = "rand_forest")
+
+ncv_experiment <- function(datasets = my_dataset_list, reps = 50, seed = 2021) {
+  bar <- new_bar(2 * length(datasets) * reps)
+  set.seed(seed)
+  for (i in seq_along(datasets)) {
+    results_list <-
+      list(rand_forest = replicate(reps, NA, simplify = FALSE),
+           naive_bayes = replicate(reps, NA, simplify = FALSE))
+    d <- datasets[[i]]
+    name <- d$name
+    tib <- d$tibble
+    X <- select(tib, -class)
+    Y <- tib$class
+
+    cat(paste("\nUsing dataset", name, "...\n"))
+    for (j in seq_len(reps)) {
+      train_indices <- sample(1:nrow(tib), d$sample_size)
+      X_train <- X[ train_indices, ]
+      X_test  <- X[-train_indices, ]
+      Y_train <- Y[ train_indices]
+      Y_test  <- Y[-train_indices]
+
+      results_list$rand_forest[[j]] <-
+        nested_cv(X_train, Y_train, rand_forest_fns, n_folds = 5, reps = 20,
+                  funcs_params = list(classes = levels(Y)))
+      model <- fitter_rand_forest(X_train, Y_train,
+                                  funcs_params = list(classes = levels(Y)))
+      results_list$rand_forest[[j]]$actual <-
+        1 - mean(predictor_rand_forest(model, X_test) == Y_test)
+      tick(bar)
+
+      results_list$naive_bayes[[j]] <-
+        nested_cv(X_train, Y_train, naive_bayes_fns, n_folds = 5, reps = 20,
+                  funcs_params = list(classes = levels(Y)))
+      model <- fitter_naive_bayes(X_train, Y_train,
+                                  funcs_params = list(classes = levels(Y)))
+      results_list$naive_bayes[[j]]$actual <-
+        1 - mean(predictor_naive_bayes(model, X_test) == Y_test)
+      tick(bar)
+    }
+
+    save_data(results_list$rand_forest,
+              paste0("ncv_rand_forest_", name, "_", seed, ".RData"))
+    save_data(results_list$naive_bayes,
+              paste0("ncv_naive_bayes_", name, "_", seed, ".RData"))
+  }
+}
--- R/plots.R
+++ R/plots.R
@@ -75,6 +75,18 @@ draw_cancer_plot <- function() {
   # ggsave("~/Documents/paper/tex/kohavi_sd.pdf", height=3, width=7)
 
   tib %>%
+    pivot_longer(c(mean, sd)) %>%
+    mutate(folds = ordered(folds, levels = c(2, 5, 10, 20, -1))) %>%
+    ggplot(aes(folds, value, group = algo, color = algo)) +
+    geom_line() +
+    facet_grid(name~dataset, scale = "free_y") +
+    scale_color_brewer(name = "Algorithm", palette = "Dark2") +
+    labs(x = "Number of folds (-1 = leave-one-out)",
+         y = "Sample standard deviation",
+         title = "Accuracy sample mean and standard deviation")
+  #ggsave("~/Documents/paper/tex/kohavi_both.pdf", height=3, width=7)
+
+  tib %>%
     filter(algo %in% c("C4.5", "XGBoost", "random forest")) %>%
     rename(SD = sd) %>%
     pivot_longer(c(mean, SD)) %>%
@@ -200,4 +212,125 @@ draw_sim_data_ex <- function() {
          subtitle = "Two-dimensional examples, with different choices of Bayes error") +
     guides(color = "none") +
     coord_fixed()
+}
+
+draw_lr_plot <- function() {
+  dataset_names <- c("chess", "mushroom")
+  folds_values <- c(2, 5, 10, 20, 200)
+  param_values <- c(0, 0.01, 0.1, 1, 10)
+  tib <- tibble(name = rep(dataset_names, each = 5 * 5),
+                folds = rep(folds_values, each = 5, times = 2),
+                param = rep(param_values, times = 5 * 2),
+                mean = NA,
+                sd   = NA)
+  for (k in seq_along(dataset_names)) {
+    seed <- c(2021, 2023)[[k]]
+    name <- dataset_names[[k]]
+    for (i in seq_along(folds_values)) {
+      folds <- folds_values[[i]]
+      ds <- read_saved_data(glue("cv_lr_{name}_{folds}_50_{seed}.RData"))
+      for (j in seq_along(param_values)) {
+        param <- param_values[[j]]
+        m <- mean(ds[[as.character(param)]])
+        s <- sd(ds[[as.character(param)]])
+        tib[25 * (k - 1) + 5 * (i - 1) + j, "mean"] <- m
+        tib[25 * (k - 1) + 5 * (i - 1) + j, "sd"]   <- s
+      }
+    }
+  }
+
+  # Making an adjustment so 1 and 10 don't totally overlap
+  tib2 <- tib
+  tib2[tib2$param == 1, "mean"] <- tib2[tib2$param == 1, "mean"] + 0.008
+  tib2[tib2$param == 1, "sd"] <- tib2[tib2$param == 1, "sd"] + 0.002
+
+  tib2 %>%
+    rename(set = name, SD = sd, Parameter = param) %>%
+    pivot_longer(c(mean, SD)) %>%
+    mutate(Parameter = ordered(Parameter),
+           folds = ordered(folds)) %>%
+    ggplot(aes(folds, value, group = Parameter, color = Parameter)) +
+    geom_line() +
+    facet_grid(name ~ set, scale = "free_y") +
+    labs(title = "The effect of regularization",
+         x = "Number of folds",
+         y = "Estimate")
+  ggsave("~/Documents/paper/tex/lr_both.pdf", height=3, width=7)
+}
+
+draw_ncv_plot <- function() {
+  dataset_names <- c("cancer", "chess", "hypothyroid", "mushroom")
+  algo_names <- c("naive_bayes", "rand_forest")
+
+  tib <-
+    tibble(set = rep(dataset_names, each = 100),
+           trial = rep(1:50, each = 2, times = 4),
+           algo = rep(algo_names, times = 200),
+           lo = NA, mid = NA, hi = NA, actual = NA, success = NA)
+
+  for (i in seq_along(dataset_names)) {
+    set <- dataset_names[[i]]
+    for (j in seq_along(algo_names)) {
+      algo <- algo_names[[j]]
+      d <- read_saved_data(glue("ncv_{algo}_{set}_2021.RData"))
+      rows <- which(tib$algo == algo & tib$set == set)
+      tib[rows, "lo" ] <- sapply(d, function(x) x$ci_lo)
+      tib[rows, "mid"] <- sapply(d, function(x) x$err_hat)
+      tib[rows, "hi" ] <- sapply(d, function(x) x$ci_hi)
+      tib[rows, "actual"] <- sapply(d, function(x) x$actual)
+      tib[rows, "success"] <- sapply(d, function(x) x$actual >= x$ci_lo & x$actual <= x$ci_hi)
+    }
+  }
+
+  tib %>%
+    mutate(algo = fct_recode(algo,
+                             "naive Bayes" = "naive_bayes",
+                             "random forest" = "rand_forest")) %>%
+    ggplot() +
+    geom_point(aes(trial, 1 - mid, color = success), size = 0.5) +
+    geom_errorbar(aes(x = trial, ymin = 1 - lo, ymax = 1 - hi, color = success),
+                  width = 0, size = 0.5) +
+    geom_point(aes(trial, 1 - actual), size = 0.5) +
+    facet_grid(algo~set, scale = "free_y") +
+    labs(title = "Confidence intervals via nested cross-validation",
+         x = "Trial", y = "Accuracy") +
+    scale_color_hue(name = "Contains holdout estimate?", labels = c("No", "Yes"))
+  ggsave("~/Documents/paper/tex/ncv.pdf", height=3, width=7)
+}
+
+draw_knn_plot <- function() {
+  d <- read_saved_data("knn.RData")
+  tib <- tibble(dims = rep(1:3, each = 6 * 3),
+                bayes_error = rep(c(0, 0.1, 0.2), each = 6, times = 3),
+                additions = rep(1:6, times = 3 * 3),
+                mean = NA,
+                sd = NA)
+  for (i in 1:3) {
+    for (j in 1:3) {
+      xs <- d[[(i - 1) * 3 + j]]
+      for (k in 1:6) {
+        ix <- (i - 1) * 6 * 3 + (j - 1) * 6 + k
+        tib[ix, "mean"] <- mean(xs[[k]])
+        tib[ix, "sd"]   <- sd(xs[[k]])
+      }
+    }
+  }
+
+  gam <- optimize(function(x) 2 * x * pnorm(x, lower.tail = FALSE), 0:1, maximum = TRUE)$objective
+
+  tib %>%
+    mutate(asymptote = 1 - bayes_error * 1.5209669,
+           upper_bound = 1 - bayes_error,
+           bayes_error = ordered(bayes_error),
+           dims = ordered(2 * dims),
+           size = additions * 500) %>%
+    rename(`Bayes error` = bayes_error, Dimension = dims) %>%
+    ggplot(aes(size, mean, color = Dimension)) +
+    geom_line() +
+    geom_hline(aes(yintercept = upper_bound)) +
+    geom_hline(aes(yintercept = asymptote), linetype = "dotted") +
+    facet_wrap(~`Bayes error`, labeller = label_both) +
+    scale_color_brewer(palette = "Set1") +
+    labs(title = "Asymptotic accuracy of 5\uadnearest neighbor classifiers", x = "Training set size", y = "Sample mean accuracy")
+  ggsave("~/Documents/paper/tex/knn.pdf", height=3, width=7)
 }
